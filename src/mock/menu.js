d92dbc971380d67117f5c1525bb73ae0b8c5f5e0
Mirabbosbek9778 <jwicj969@gmail.com>
2024-01-11 21:08:16 +0500

    full project and mock and components

---
+++ src/mock/menu.js
@@ -0,0 +1,27 @@
+export const Menu = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/#",
+  },
+  {
+    id: 2,
+    name: "Top Rated",
+    link: "/#services",
+  },
+  {
+    id: 3,
+    name: "Kids Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Mens Wear",
+    link: "/#",
+  },
+  {
+    id: 3,
+    name: "Electronics",
+    link: "/#",
+  },
+];
